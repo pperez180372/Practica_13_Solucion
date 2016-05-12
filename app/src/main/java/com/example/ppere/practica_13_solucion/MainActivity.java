0ae59c00061502169da8a433b0013b0440f8172c
pperez180372 <pperez180372@gmail.com>
2016-05-12 10:08:48 +0200

    segundaui2ddd

---
+++ app/src/main/java/com/example/ppere/practica_13_solucion/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.ppere.practica_13_solucion;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
